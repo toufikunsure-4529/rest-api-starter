719f48d5fb2c480e95300d52c8a973c6477cb4b8
TOUFIK SK <toufiksk.ab3@gmail.com>
2023-09-19 22:40:50 +0530

    added api call and handle error

---
+++ app.js
@@ -0,0 +1,32 @@
+let url="https://kontests.net/api/v1/all"
+let cardContainer=document.getElementById("cardContainer")
+let response= fetch(url)
+response.then((contests)=>{
+  return contests.json();
+}).then((contests)=>{
+  console.log(contests)
+  let ihtml=''
+  for(let item in contests){
+    // console.log(contests[item])
+    ihtml+=`
+    <div class="card mx-2 my-2" style="width: 22rem;">
+    <img src="https://source.unsplash.com/1600x900/?contests" class="card-img-top" alt="card-img-">
+    <div class="card-body">
+      <h6 class="card-title">${contests[item].name}</h6>
+      <p class="card-text">Status: ${contests[item].status}</p>
+      <p class="card-text">in_24_hours? ${contests[item].in_24_hours}</p>
+      <p class="card-text">Site: ${contests[item].site}</p>
+      <p class="card-text">Start at: ${contests[item].start_time}</p>
+      <p class="card-text">End at: ${contests[item].end_time}</p>
+      <a href="${contests[item].url}" class="btn btn-primary my-4" target="_blank">Visit here</a>
+    </div>
+  </div>
+    `
+    cardContainer.innerHTML=ihtml
+  }
+},(Error)=>{
+  cardContainer.innerHTML="<h3>Some error occurred while processing your request.Please try again later</h3>"
+})
+
+
+
